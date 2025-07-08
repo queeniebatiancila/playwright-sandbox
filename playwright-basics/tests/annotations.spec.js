a8ae3a464f35db1eafafe9c6d154b0e84a66d3aa
Queenie Joy Batiancila <queeniebatiancila@gmail.com>
2025-07-08 21:12:33 +0800

    Add Playwright Basics Folder

---
+++ playwright-basics/tests/annotations.spec.js
@@ -0,0 +1,41 @@
+import { test, expect } from '@playwright/test';
+
+test.skip('Skip the test', async ({ page }) => {
+    // Test will be skipped
+});
+
+test('Not yet ready', async ({ page }) => {
+    test.fail();
+    // Will mark test as failure, will show error if test does not fail
+});
+
+test.fixme('Test to be fixed', async ({ page }) => {
+});
+
+test('Slow test', async ({ page }) => {
+    test.slow()
+});
+
+test.only('Focus this test', async ({ page }) => {
+    // Run only focused tests in the entire project
+});
+
+test('Test login page @smoke', async ({ page }) => {
+    // Run only focused tests in the entire project
+});
+
+
+// You can tag your tests with tags like
+// @smoke
+// @sanity
+// @fast  @slow 
+// and only run the tests that have the certain tag
+
+// test('Test full report @smoke, async ({ page }) =＞ {
+//    // ...
+// });
+
+// npx playwright test --grep “@smoke”
+
+// Opposite of grep  Skip tests with certain tag
+// npx playwright test --grep-invert “@smoke”
